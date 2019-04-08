b60d4cdffa0a580549c441d0051380bd95b6e0fd
hppod <hppodgornik@gmail.com>
2019-04-07 23:15:38 -0300

    Trabalhando com objetos no JEST

--- test/jest.test.js
+++ test/jest.test.js
@@ -8,3 +8,14 @@ test('Devo conhecer as principais assertivas do JEST', () => {
     expect(number).toBeGreaterThan(9)
     expect(number).toBeLessThan(11)
 })
+
+test('Devo saber trabalhar com objetos', () => {
+    const obj = {name: 'João', mail: 'joao@mail.com'}
+    expect(obj).toHaveProperty('name')
+    expect(obj).toHaveProperty('name', 'João')
+    expect(obj.name).toBe('João')
+
+    const obj2 = {name: 'João', mail: 'joao@mail.com'}
+    expect(obj).toEqual(obj2)
+    expect(obj).toBe(obj)
+})
