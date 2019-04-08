0599647168bb35c8cd515e538ef29adfeeb31740
hppod <hppodgornik@gmail.com>
2019-04-07 23:34:24 -0300

    Implementando o server e testando

---
+++ test/server.test.js
@@ -0,0 +1,9 @@
+const supertest = require('supertest')
+const request = supertest('http://localhost:3001')
+
+test('Deve responder na porta 3001', () => {
+    //acessar a URL http://localhost:3001
+    //verificar que a resposta foi 200
+    return request.get('/')
+        .then(res => expect(res.status).toBe(200));
+})
