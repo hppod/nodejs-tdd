336052abe6b21edcb7e3771946b0e59662a53d4c
hppod <hppodgornik@gmail.com>
2019-05-12 23:08:17 -0300

    Setando consign verbose = false

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 const consign = require('consign')
 
-consign({ cwd: 'src' })
+consign({ cwd: 'src', verbose: false })
     .include('./config/middlewares.js')
     .into(app)
 
